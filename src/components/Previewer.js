905bacb8bfdf18063d3e1d8ee3957c31e6040164
barishm <92534684+barishm@users.noreply.github.com>
2023-12-29 16:32:28 +0200

    Update Previewer.js

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -4,10 +4,7 @@ import Markdown from 'marked-react';
 
 const Previewer = () => {
     const { text } = useSelector((state) => state.editor);
-
-
-
-
+    
     return(
     <div className='previewWrap'>
         <div class="toolbar"><i class="fa fa-free-code-camp" title="no-stack-dub-sack"></i>Previewer</div>
